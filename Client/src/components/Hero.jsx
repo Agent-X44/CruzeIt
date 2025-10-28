48dc6726cbd29efe077dafed32ecc0ffb197c67d
NJ <147250418+Agent-X44@users.noreply.github.com>
2025-10-28 21:40:00 +0800

    hero filter and search bar spacing adjustment

--- Client/src/components/Hero.jsx
+++ Client/src/components/Hero.jsx
@@ -92,7 +92,7 @@ const Hero = () => {
         onSubmit={handleSearch} 
         className="flex flex-col md:flex-row items-start md:items-center justify-between py-3 px-4 md:py-4 md:px-6 rounded-xl md:rounded-full w-[95%] max-w-[56rem] bg-white shadow-xl mx-4"
       >
-        <div className="flex flex-col md:flex-row items-start md:items-center gap-4 md:gap-6 lg:gap-10 w-full md:ml-6">
+         <div className="flex flex-col md:flex-row items-start md:items-center gap-4 md:gap-4 lg:gap-6 w-full md:ml-6 md:flex-1">
           {/* Pickup Location */}
           <div className="flex flex-col items-start gap-1.5 md:gap-2 w-full md:w-auto relative" ref={dropdownRef}>
             <label className="text-xs md:text-sm font-medium text-gray-700">Pickup Location</label>
